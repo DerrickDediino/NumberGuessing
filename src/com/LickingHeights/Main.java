07736ba55da069d8896b36a0f224df8363129447
Derrick <Derrick.Dedino.2022@student.lhschools.org>
2019-04-01 14:19:59 -0400

    Coded for user input and random generated number

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,22 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("This is the number guessing game. Pick a number between 1 and 100 inclusive.");
+
+        Scanner keyboard = new Scanner(System.in);
+        Integer number = keyboard.nextInt();
+
+        int computerNumber = (int) (Math.random()*100);
+
+       do{
+           System.out.println("Your number is too high.");
+       }while (number>computerNumber);
+
+
+
     }
 }
