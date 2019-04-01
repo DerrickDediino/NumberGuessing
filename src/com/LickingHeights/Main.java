559a43397a8ff08b2d38a45f4a39268c72f2d075
Derrick <Derrick.Dedino.2022@student.lhschools.org>
2019-04-01 13:46:55 -0400

    Created Intellij Project and asked for user input

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,8 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is the number guessing game. Pick a number between 1 and 100 inclusive.");
+    }
+}
